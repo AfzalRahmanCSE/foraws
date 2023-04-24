0762671270c66e4cee244302dea59893185bdb0d
AfzalUrRahman <afzalrahman.cse@gmail.com>
2023-04-24 17:08:52 +0530

    Header Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import React from 'react'
+import Header from './Header';
+import Footer from './Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import NotFound from './NotFound';
 const App=()=>{
-    return <h1>Welcome to React App</h1>
+    return <div>
+        <BrowserRouter>
+        <Header/>
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/contact' element={<Contact/>}/>    
+
+            <Route path='*' element={<NotFound/>}/>
+        </Routes>
+        <hr/>
+        <Footer/>
+        </BrowserRouter>
+
+    </div>
 }
 
 export default App;
---
+++ src/Footer.js
@@ -0,0 +1,4 @@
+const Footer=()=>{
+return <div>&copy; Website Created and Maintained by AUR,2023</div>
+}
+export default Footer;
